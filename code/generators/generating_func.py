3ac7641fa0ccfab315ab226b147ddca0b5b0781e
Misha Padidar <map454@cornell.edu>
2021-08-11 13:11:14 -0700

    generators

---
+++ code/generators/generating_func.py
@@ -0,0 +1,19 @@
+import abc
+from abc import ABC, abstractmethod
+
+class GeneratingFunction(object):
+    """Base class for generating functions."""
+
+    __metaclass__ = abc.ABCMeta
+
+    def __call__(self,xx):
+      return self.eval(xx)
+
+    @abstractmethod
+    def eval(self):  # pragma: no cover
+        pass
+
+    @abstractmethod
+    def jac(self):  # pragma: no cover
+        pass
+
---
+++ code/generators/sigmoid.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+
+from generating_func import GeneratingFunction
+
+class Sigmoid(GeneratingFunction):
+    """Componentwise Sigmoid function: s(x) = 1/(1+e^(-sigma*x))
+    """
+
+    def __init__(self, sigma=1):
+        """
+        Parameter sigma can be positive float or np.array
+        with same length as inputs x.
+        """
+        assert np.all(sigma >= 0), "sigma must be non-negative"
+        self.sigma = sigma 
+
+    def eval(self, x):
+        """Evaluate the sigmoid at x
+        :param x: Data point
+        :type x: numpy.array
+        :return: Value at x
+        :rtype: numpy.array
+        """
+        return 1/(1+np.exp(-self.sigma*x))
+
+    def jac(self, x):
+        """Evaluate the sigmoid jacobian at x
+        :param x: Data point
+        :type x: numpy.array
+        :return: Value at x
+        :rtype: 2D numpy.array
+        """
+        return np.diag(self.sigma*np.exp(-self.sigma*x)/(1+np.exp(-self.sigma*x))**2)
+
