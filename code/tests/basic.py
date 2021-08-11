f3f586c5ca3471f9b1b4d0b6bfd4705be791ed04
Misha Padidar <map454@cornell.edu>
2021-08-11 15:16:27 -0700

    basic test script

---
+++ code/tests/basic.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.optimize import minimize
+import sys
+sys.path.append("../generators/")
+sys.path.append("../problems/")
+sys.path.append("../utils/")
+from sigmoid import Sigmoid
+from quadratic1 import Quadratic1
+from rescale import *
+
+dim  = 2
+f    = Quadratic1(dim)
+lb   = f.lb
+ub   = f.ub
+# generator
+sig = Sigmoid()
+# merit
+ft = lambda xx: f(from_unit_cube(sig(xx),lb,ub))
+jac = lambda xx: sig.jac(xx) @ np.diag(ub-lb) @ f.grad(sig(xx))
+
+# optimize
+x0 = np.random.randn(dim)
+res = minimize(ft,x0,jac=jac,method='BFGS',options={'gtol':1e-8})
+xopt = res.x
+print(res)
+print("Minima Found is ",from_unit_cube(sig(xopt),lb,ub))
+print("Distance to Optima: ",np.linalg.norm(from_unit_cube(sig(xopt),lb,ub) - f.minimum))
