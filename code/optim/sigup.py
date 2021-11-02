fixed computation of lagrange multipliers. incorrectly used unit cube bounds in kkt system rather than lb,ub

code/optim/sigup.py
@@ -3,6 +3,7 @@ from scipy.optimize import minimize,Bounds
 import sys
 sys.path.append("../generators/")
 sys.path.append("../utils/")
+from compute_lagrange import compute_lagrange
 from sigmoid import Sigmoid
 from rescale import *
 
@@ -54,7 +55,7 @@ def sigup(f,f_grad,lb,ub,y0,sigma0 = 1.0,eps = 1e-8,delta=1e-10,gamma=10.0,metho
     yopt = gen(xopt)
     # compute distance to boundary
     eta = np.minimum(yopt,1-yopt)
-    # y^* in regular domain
+    # zopt = y^* in original domain [lb,ub]
     zopt = from_unit_cube(yopt,lb,ub)
     g_z  = f_grad(zopt)
 
@@ -70,16 +71,16 @@ def sigup(f,f_grad,lb,ub,y0,sigma0 = 1.0,eps = 1e-8,delta=1e-10,gamma=10.0,metho
     if np.all(np.abs(g_z)< eps): # exit if gradient is flat
       return zopt
     else: # check the KKT conditions
-      lam,mu = compute_lagrange(zopt,g_z)
-      kkt = np.all(np.abs(g_z + lam - mu)< eps) and np.all(np.abs(lam*zopt) < eps) and np.all(np.abs(mu*(1-zopt))< eps)
+      lam,mu = compute_lagrange(zopt,g_z,lb,ub)
+      kkt = np.all(np.abs(g_z + lam - mu)< eps) and np.all(np.abs(lam*(zopt-lb)) < eps) and np.all(np.abs(mu*(ub-zopt))< eps)
       if kkt: 
         print('')
         print('stationary')
         print(np.abs(g_z + lam - mu))
         print('complementary slackenss lambda*y')
-        print(np.abs(lam*zopt))
+        print(np.abs(lam*(zopt-lb)))
         print('complementary slackenss mu*(1-y)')
-        print(np.abs(mu*(1-zopt)))
+        print(np.abs(mu*(ub-zopt)))
         return zopt
 
     if np.all(sigma > cap_sigma-1):
@@ -87,7 +88,7 @@ def sigup(f,f_grad,lb,ub,y0,sigma0 = 1.0,eps = 1e-8,delta=1e-10,gamma=10.0,metho
       return zopt
 
     # update sigma
-    eta[eta<1e-8] = cap_eta # cap the update size
+    eta[eta<cap_eta] = cap_eta # cap the update size
     sigma = gamma*sigma/np.sqrt(eta)
     sigma = np.minimum(sigma,cap_sigma)
     # reset for next iteration
@@ -96,31 +97,31 @@ def sigup(f,f_grad,lb,ub,y0,sigma0 = 1.0,eps = 1e-8,delta=1e-10,gamma=10.0,metho
   return zopt
 
 
-def compute_lagrange(y,g):
-  """
-  Solve the QP to check approximate stationarity
-  y: point y
-  g: grad f(y)
-  """
-  # sizes
-  dim_y = len(y)
-  n_con = 2*dim_y
-
-  # setup the QP
-  I = np.eye(dim_y)
-  M = np.hstack((I + np.outer(y,y), -I))
-  M2 = np.hstack((-I, I + np.outer(1-y,1-y)))
-  M = np.vstack((M,M2))
-  b = np.hstack((g,-g))
-  f = lambda z: 0.5 * z @ M @ z + z @ b
-  jac = lambda z: M @ z + b
-
-  # solve
-  bounds = Bounds(-np.inf*np.ones(n_con),np.zeros(n_con))
-  res = minimize(f,np.ones(n_con),jac=jac,method="L-BFGS-B",bounds=bounds,options={'gtol':1e-8})
-  z = res.x
-  lam = z[:dim_y]
-  mu = z[dim_y:]
-
-  return lam,mu
+#def compute_lagrange(y,g):
+#  """
+#  Solve the QP to check approximate stationarity
+#  y: point y
+#  g: grad f(y)
+#  """
+#  # sizes
+#  dim_y = len(y)
+#  n_con = 2*dim_y
+#
+#  # setup the QP
+#  I = np.eye(dim_y)
+#  M = np.hstack((I + np.outer(y,y), -I))
+#  M2 = np.hstack((-I, I + np.outer(1-y,1-y)))
+#  M = np.vstack((M,M2))
+#  b = np.hstack((g,-g))
+#  f = lambda z: 0.5 * z @ M @ z + z @ b
+#  jac = lambda z: M @ z + b
+#
+#  # solve
+#  bounds = Bounds(-np.inf*np.ones(n_con),np.zeros(n_con))
+#  res = minimize(f,np.ones(n_con),jac=jac,method="L-BFGS-B",bounds=bounds,options={'gtol':1e-8})
+#  z = res.x
+#  lam = z[:dim_y]
+#  mu = z[dim_y:]
+#
+#  return lam,mu
 

code/optim/test_sigup.py
@@ -7,23 +7,23 @@ from rosenbrock import Rosenbrock
 from convex_quadratic import ConvexQuadratic
 
 dim  = 2
-#f = Rosenbrock(dim)
-# f    = Quadratic1(dim)
-#lb   = f.lb
-#ub   = f.ub
-lb = np.zeros(dim)
-ub = np.ones(dim)
 
+# problem 1
+f = Rosenbrock(dim)
+
+# problem 2
 #yopt = np.ones(dim) 
-yopt = np.ones(dim) + np.array([-1e-7,1e-1])
+#yopt = np.ones(dim) + np.array([-1e-7,1e-1])
 #yopt = np.ones(dim) - np.array([1e-7,1e-1])
 #yopt = np.ones(dim) - np.array([0.5,0.0])
 #yopt = np.ones(dim) + np.array([0.5,0.0])
-A = np.diag(np.array([100,2]))
-f = ConvexQuadratic(lb,ub,A,yopt)
+#A = np.diag(np.array([100,2]))
+#lb = np.zeros(dim)
+#ub = np.ones(dim)
+#f = ConvexQuadratic(lb,ub,A,yopt)
 
 # optimizer params
-y0 = (lb+ub)/2
+y0 = (f.lb+f.ub)/2
 n_solves = 10
 gamma = 1.0
 eps = 1e-9
@@ -31,7 +31,7 @@ method = 'BFGS'
 verbose=True
 
 # optimize
-z = sigup(f,f.grad,lb,ub,y0,eps = eps,gamma=gamma,method=method,verbose=verbose)
+z = sigup(f,f.grad,f.lb,f.ub,y0,eps = eps,gamma=gamma,method=method,verbose=verbose)
 
 print("Optimal Value is ",f(z))
 print("Minima Found is ",z)