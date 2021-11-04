0482aa5c2cf18c45bfdbc33ba05461b0f1397340
mishapadidar <map454@cornell.edu>
2021-11-04 12:16:41 -0400

    added nlopt

--- code/tests/cutest_sweep/run_exp.py
+++ code/tests/cutest_sweep/run_exp.py
@@ -4,6 +4,7 @@ import pycutest
 import os
 import sys
 from scipy.optimize import minimize,Bounds
+import nlopt
 import pandas as pd
 # our libs
 sys.path.append("../../generators/")
@@ -27,18 +28,22 @@ problems = pd.read_pickle("../../problems/cutest_problems.pickle")
 sig = Sigmoid()
 
 # sigup parameters
-sig_sigma0 = 1.0
-sig_eps = 1e-8
+sig_sigma0 = 0.1
+sig_eps = 0.0 # set to zero for infinite run
 sig_gamma = 1.0
 sig_method = "BFGS"
 
 # LBFGS params
 gtol = sig_eps
+ftol = 0.0 # for infinite run
+maxfun = int(1e6)
+maxiter = int(1e6)
 
 for pname in problems['name']:
   print(f"\nproblem: {pname}")
   problem_data = {}
   problem_data['problem'] = pname
+  problem_data['runs'] = [] # list to contain runs
 
   # load problem
   prob = pycutest.import_problem(pname)
@@ -48,14 +53,28 @@ for pname in problems['name']:
   obj = prob.obj
   grad = lambda xx: prob.obj(xx,gradient=True)[1]
 
+  # check y0 and bounds
+  assert np.all(lb < ub), "bound violation lb > ub"
+  if np.any(lb >= y0) or np.any(y0>= ub):
+    # strict interior point
+    y0 = (lb+ub)/2
+  assert np.all(lb < y0) and np.all(y0 < ub), "y0 is infeasible"
+
   # construct the merit function
   ft = lambda xx: obj(from_unit_cube(sig(xx),lb,ub))
   ft_grad = lambda xx: sig.jac(xx) @ np.diag(ub-lb) @ grad(from_unit_cube(sig(xx),lb,ub))
 
+  # collect the dimension
+  problem_data['dim'] = dim
+
   # call sigup
   method = 'sigup'
   func = eval_wrapper(obj,dim)
-  z = sigup(func,grad,lb,ub,y0,sigma0=sig_sigma0,eps =sig_eps,gamma=sig_gamma,method=sig_method,verbose=False)
+  try:
+    z = sigup(func,grad,lb,ub,y0,sigma0=sig_sigma0,eps =sig_eps,gamma=sig_gamma,method=sig_method,verbose=False)
+  except:
+    z = func.X[-1]
+    pass
   fopt = obj(z)
   X = func.X
   fX = func.fX
@@ -68,13 +87,32 @@ for pname in problems['name']:
   method_data['gamma'] = sig_gamma
   method_data['eps'] = sig_eps
   method_data['submethod'] = sig_method
-  problem_data[method] = method_data
+  problem_data['runs'].append(method_data)
 
   # projected gradient
   method = "L-BFGS-B"
   func = eval_wrapper(obj,dim)
-  res = minimize(func,y0,jac=grad,method=method,options={'gtol':gtol})
-  z = res.x
+
+  # nlopt objective
+  def objective_with_grad(x,g):
+    f = func(x)
+    g[:] = grad(x)
+    return f
+  opt = nlopt.opt(nlopt.LD_LBFGS, dim)
+  opt.set_min_objective(objective_with_grad)
+  opt.set_lower_bounds(lb)
+  opt.set_upper_bounds(ub)
+  opt.set_maxeval(maxfun)
+  opt.set_ftol_rel(ftol)
+  opt.set_ftol_abs(ftol)
+  opt.set_xtol_rel(ftol)
+  try:
+    #res = minimize(func,y0,jac=grad,method=method,options={'gtol':gtol,'ftol':ftol,'maxiter':maxiter,'maxfun':maxfun})
+    #z = res.x
+    z = opt.optimize(y0)
+  except:
+    z = func.X[-1]
+    pass
   fopt = obj(z)
   X = func.X
   fX = func.fX
@@ -84,7 +122,7 @@ for pname in problems['name']:
   method_data['X'] = X
   method_data['fX'] = fX
   method_data['gtol'] = gtol
-  problem_data[method] = method_data
+  problem_data['runs'].append(method_data)
 
   # TODO: call augmented lagrangian
 
