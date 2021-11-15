8c72b91341f5132cb4aa4f735f46bc98f6af4095
mishapadidar <map454@cornell.edu>
2021-11-15 14:50:39 -0500

    post processing now uses new kkt method.

--- code/tests/cutest_sweep/post_process_traj.py
+++ code/tests/cutest_sweep/post_process_traj.py
@@ -4,7 +4,8 @@ import pickle
 import pycutest
 import sys
 sys.path.append("../../utils/")
-from compute_lagrange import compute_lagrange
+#from compute_lagrange import compute_lagrange
+from check_kkt import check_kkt,compute_kkt_tol
 
 # find the data files
 data_loc = "./data/*.pickle"
@@ -32,15 +33,24 @@ for ff in filelist:
     # storage for worst kkt violation
     KKT = np.zeros(len(X))
 
+    # kkt tolerance guess
+    kkt = 1.0
+
     for jj,z in enumerate(X):
      
       # compute the gradient at z
       g_z = prob.obj(z,gradient=True)[1]
       # get the bounds
       lb,ub = prob.bl,prob.bu
+
       # compute the kkt system at each point
-      lam,mu = compute_lagrange(z,g_z,lb,ub)
-      kkt = np.max([np.abs(g_z + lam - mu),np.abs(lam*(z-lb)),np.abs(mu*(ub-z))])
+      #lam,mu = compute_lagrange(z,g_z,lb,ub)
+      #kkt = np.max([np.abs(g_z + lam - mu),np.abs(lam*(z-lb)),np.abs(mu*(ub-z))])
+
+      # compute the kkt satisfaction with binary search
+      if kkt==np.inf or kkt < 1e-8:
+        kkt = 1.0 # ensure safe start to binary search
+      kkt = compute_kkt_tol(z,g_z,lb,ub,eps=kkt)
 
       # store the value
       KKT[jj] = kkt
@@ -49,7 +59,7 @@ for ff in filelist:
     g_z = prob.obj(X[0],gradient=True)[1]
     method_data["grad_x0"] = np.copy(g_z)
     # save the data
-    method_data["KKT"] = KKT
+    method_data["KKT"] = np.copy(KKT)
     runs[ii]  = method_data
   
   # save the data
